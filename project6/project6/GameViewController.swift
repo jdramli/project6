901852f2f1362a5f132ad3d043bb9382484beb3c
Joseph Ramli <jramli1@binghamton.edu>
2019-11-12 23:28:33 -0500

    updated storyboard andbackground and copied project 5 files

--- project6/project6/GameViewController.swift
+++ project6/project6/GameViewController.swift
@@ -1,8 +1,8 @@
 //
 //  GameViewController.swift
-//  project6
+//  project5
 //
-//  Created by Joseph Daniel Ramli on 11/12/19.
+//  Created by Joseph Daniel Ramli on 10/30/19.
 //  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
 //
 
@@ -37,9 +37,15 @@ class GameViewController: UIViewController {
                     
                     view.showsFPS = true
                     view.showsNodeCount = true
+                    view.showsPhysics = true // Added this to show physics body outlines
                 }
             }
         }
+    }
+    @IBAction func unwindToGameViewController(segue: UIStoryboardSegue){
+         //Nothing is needed here
+         //These comments are just instructions on how to edit the "Main.storyboard" file after adding this function action to the code here.  Adding this function action makes seeing this possible when doing "control" click-and-drag to the "EXIT" button on the top of any viewcontroller in the storyboard
+         //After this function is added, drage the segue from your button up to the little orange "EXIT" icon at the top right of your sourceview of the viewcontroller in the Main.storyboard, and you should see the option to "unwindTo[ViewControllerName]WithSegue" i.e. "unwindToStartViewControllerWithSegue"
     }
 
     override var shouldAutorotate: Bool {
