acfdedb1adb28f074d419d81d82e5f37bdf1002b
Joseph Ramli <jramli1@binghamton.edu>
2019-11-13 10:41:19 -0500

    Added a tabview controller and changed background colors to confirm links

---
+++ project6/project6/FirstTabViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstTabViewController.swift
+//  project6
+//
+//  Created by Joseph Daniel Ramli on 11/13/19.
+//  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
+//
+
+import UIKit
+
+class FirstTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ project6/project6/SecondTabViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondTabViewController.swift
+//  project6
+//
+//  Created by Joseph Daniel Ramli on 11/13/19.
+//  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
+//
+
+import UIKit
+
+class SecondTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.purple
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
