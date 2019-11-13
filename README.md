9cfda034e835be9a6f42457ad01cb97839f397a4
Joseph Ramli <jramli1@binghamton.edu>
2019-11-12 22:31:48 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project6
+
+
+Project 6 goals: add database retrieval and create multiple highscore views to project 5, i.e. Create a separate view of database high scores vs. local high scores.
---
+++ project6/project6Tests/project6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  project6Tests.swift
+//  project6Tests
+//
+//  Created by Joseph Daniel Ramli on 11/12/19.
+//  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
+//
+
+import XCTest
+@testable import project6
+
+class project6Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
