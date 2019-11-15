9ef8b6209c954e6d52b03d5e734c45ec4f4557a0
Joseph Ramli <jramli1@binghamton.edu>
2019-11-15 10:45:22 -0500

    Added the ability to load the website, getting configuration error message regarding info plist

--- project6/project6/FirstTabViewController.swift
+++ project6/project6/FirstTabViewController.swift
@@ -8,24 +8,30 @@
 
 import UIKit
 
+
 class FirstTabViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.lightGray
+        if let website = URL(string: "http://cs.binghamton.edu/~pmadden/courses/441score/getscores.php") {
+            do{
+                var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+                label.center = CGPoint(x: 160, y: 284)
+                label.textAlignment = NSTextAlignment.center
+                label.text = "Website loaded!"
+                self.view.addSubview(label)
+                let contents = try String(contentsOf: website)
+                print(contents)
+            } catch{
+                print("Website did not load properly")
+            }
+        }
+        else{
+            print("url was bad")
+        }
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
